cb5712a63b207470dd76fd5974d16ae049dcd3ab
Ashwini130 <ashwini130297@gmail.com>
2018-02-20 00:33:22 +0530

    Update

--- app/src/androidTest/java/com/example/border/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/border/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.top_border", appContext.getPackageName());
+        assertEquals("com.example.border", appContext.getPackageName());
     }
 }
